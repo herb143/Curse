ae8ec360f76ee597ad85de52b8e8e99dfca7e957
1000000000 <lthyde@gmail.com>
2012-09-12 08:28:36 -0400

    Made CurseTranslator create way better pig latin

--- src/me/hgilman/Curse/CurseTranslator.java
+++ src/me/hgilman/Curse/CurseTranslator.java
@@ -1,13 +1,20 @@
 package me.hgilman.Curse;
 
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class CurseTranslator {
 
-	private static char[] vowels ={'a', 'e', 'i', 'o', 'u'};
+	private static char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+	private static char[] extendedVowels = {'a', 'e', 'i', 'o', 'u', 'y'};
 	private  StringTokenizer tokenizer;
 	private String word;
 	private String returnValue;
+	
+	static {
+		Arrays.sort(vowels);
+		Arrays.sort(extendedVowels);
+	}
 	
 	public CurseTranslator(String text)
 	{
@@ -30,22 +37,24 @@ public class CurseTranslator {
 	private static String shifter(String text)
 	{
 	//	text = tokenizer.nextToken();
-		int length = text.length();
 	//	System.out.println(length);
-		for(int x = 0; x < vowels.length; x++)
-		{
-			if(text.charAt(0) == vowels[x])
-			{
+		if(Arrays.binarySearch(vowels, text.charAt(0)) >= 0) {
 		//		System.out.println("vowel found");
 				text+="yay";
 				return text;
-			}
+		} else if(text.toLowerCase().charAt(0) == 'q' && text.toLowerCase().charAt(1) == 'u') {
+			text = text.substring(2);
+			text += "quay";
+			return text;
 		}
-		//System.out.println("flag1");
+		int i = 1;
 		text += text.charAt(0);
-		//System.out.println("0 pos is" + text.charAt(0));
-		
-		text = text.substring(1, length);
+		for(; Arrays.binarySearch(extendedVowels, text.charAt(i)) < 0; i++) {
+			//System.out.println("flag1");
+			text += text.charAt(i);
+			//System.out.println("0 pos is" + text.charAt(0));
+		}
+		text = text.substring(i);
 		text+="ay";
 		return text;
 	}
